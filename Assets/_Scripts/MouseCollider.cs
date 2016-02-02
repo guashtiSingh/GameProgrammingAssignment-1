4cb31d846f407ae56f5fadc60dfd3aa43fb4bd2d
Guashti Singh <gsing540@my.centennialcollege.ca>
2016-02-02 11:40:11 -0500

    Added AudioSources and Collider script

--- Assets/_Scripts/MouseCollider.cs
+++ Assets/_Scripts/MouseCollider.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class MouseCollider : MonoBehaviour {
 
+	//Private Instance variables
+	private AudioSource[] _audioSources;
+	private AudioSource _cheeseSound;
+	private AudioSource _catSound;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		this._audioSources = gameObject.GetComponents<AudioSource> ();
+		this._catSound = this._audioSources [1];
+		this._cheeseSound = this._audioSources[2];
 	}
 	
 	// Update is called once per frame
@@ -15,11 +23,11 @@ public class MouseCollider : MonoBehaviour {
 
 	public void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag ("Cheese")) {
-
+			this._cheeseSound.Play ();
 		}
 
 		if (other.gameObject.CompareTag ("Cat")) {
-
+			this._catSound.Play ();
 		}
 	}
 }
