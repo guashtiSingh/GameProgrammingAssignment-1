b1382adee9a417fe825c48d977f2316917354979
Guashti Singh <gsing540@my.centennialcollege.ca>
2016-02-02 11:14:13 -0500

    Collider Added, RigidBody2D Added

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	//Public Instance Variables
+
+	public int catNumber = 3;
+
+	public CatController cat;
+
+	// Use this for initialization
+	void Start () {
+		this._initialize ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void _initialize (){
+		for (int catCount = 0; catCount < this.catNumber; catCount++) {
+			Instantiate (cat.gameObject);
+		}
+	}
+}
---
+++ Assets/_Scripts/MouseCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Cheese")) {
+
+		}
+
+		if (other.gameObject.CompareTag ("Cat")) {
+
+		}
+	}
+}
